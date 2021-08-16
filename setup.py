0914f1c23a362540ace3688737120ee84819b965
Lisa Krombholz <lisa.krombholz@jambit.com>
2021-08-16 17:24:25 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='CRMVSC',
+   version='1.0',
+   description='Co-Regularized Multi-View Spectral Clustering',
+   packages=['CRMVSC'],
+   install_requires=['numpy', 'scipy', 'scikit-learn', 'pandas'],
+)
